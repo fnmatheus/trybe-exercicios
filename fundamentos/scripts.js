92752a120724a587ef0a277629ccf3acc53e280d
Matheus Nascimento <nasc.matheusfrancisco@gmail.com>
2022-05-18 10:40:51 -0300

    Parte II ate o exercicio 4

--- fundamentos/scripts.js
+++ fundamentos/scripts.js
@@ -67,3 +67,82 @@ console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros fav
 console.log('// Parte II');
 // Exercicio 1
 console.log('// Exercicio 1');
+function verificaPalindrome(word) {
+  let palindrome = '';
+  for(let index = word.length - 1; index >= 0; index -= 1) {
+    palindrome += word[index];
+  };
+  if(palindrome === word) {
+    return true;
+  }
+  else return false;
+};
+
+console.log(verificaPalindrome('arara'));
+console.log(verificaPalindrome('desenvolvimento'));
+// Exercicio 2
+console.log('// Exercicio 2');
+function bigestIndex(array) {
+  let bigIndex = null;
+  let bigNumber = null;
+  for(let index in array) {
+    if(array[index] > bigIndex) {
+      bigNumber = array[index];
+      bigIndex = index;
+    };
+  };
+  return bigIndex;
+};
+
+console.log(bigestIndex([2, 3, 6, 7, 10, 1]));
+// Exercicio 3
+console.log('// Exercicio 3');
+function smallerIndex(array) {
+  let smallIndex = null;
+  let smallNumber = null;
+  for(let index in array) {
+    if(array[index] < smallIndex) {
+      smallNumber = array[index];
+      smallIndex = index;
+    };
+  };
+  return smallIndex;
+};
+
+console.log(smallerIndex([2, 4, 6, 7, 10, 0, -3]));
+// Exercicio 4
+console.log('// Exercicio 4');
+function bigestName(array) {
+  let bigName = '';
+  for (let index of array) {
+    if(index.length > bigName.length) {
+      bigName = index;
+    };
+  };
+  return bigName;
+};
+
+console.log(bigestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+// Exercicio 5
+console.log('// Exercicio 5');
+function moreRepited(array) {
+  let numberRepetition = repetition(array);
+  let moreRepitedNumber = numberRepetition[Object.keys(numberRepetition)[0]]
+  for(let index in numberRepetition){
+  }
+  return moreRepitedNumber;
+};
+function repetition(array) {
+  let numberRepetition = {};
+  for(let number = 0; number < array.length; number += 1) {
+    numberRepetition[array[number]] = 0;
+    for(let index = 0; index < array.length; index += 1) {
+      if(array[index] == array[number]) {
+        numberRepetition[array[number]] += 1
+      };
+    };
+  };
+  return numberRepetition;
+}
+
+console.log(moreRepited([2, 3, 2, 5, 8, 2, 3]));
